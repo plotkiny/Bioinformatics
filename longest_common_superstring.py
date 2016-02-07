a3f2bd7eaa59b80b7950a22513d2291e945db5eb
plotkiny <plotkiny@gmail.com>
2016-02-06 16:40:10 -0800

    More functions

---
+++ longest_common_superstring.py
@@ -0,0 +1,44 @@
+from functions import read_fasta
+import numpy as np
+
+class superstring():
+        
+    def substr_in_all(self,arr, part):
+      for dna in arr:
+        if part not in dna:
+          return False
+      return True
+    
+    def common_substr(self,arr, l):
+      first = arr[0]
+      for i in range(len(first)-l+1):
+        part = first[i:i+l]
+        if self.substr_in_all(arr, part):
+          return part
+      return ""
+    
+    def longest_common_substr(self,arr):
+      l = 0; r = len(arr[0])
+    
+      while l+1<r:
+        mid = (l+r) // 2
+        if self.common_substr(arr, mid)!="":
+          l=mid
+        else:
+          r=mid
+    
+      return self.common_substr(arr, l)
+
+
+def main():       
+    file = 'rosalind_lcsm2.txt'
+    seq_dict = read_fasta(file)
+    array = np.chararray((1,0))
+    for k,v in seq_dict.items():
+        array = np.append(v,array)
+    
+    lcs = superstring()
+    return lcs.longest_common_substr(array)
+      
+main()
+      
