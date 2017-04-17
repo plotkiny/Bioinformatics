7d68415a25fbf4e0be4363aa404ffe2757b9954d
Yuri Plotkin <plotkiny@gmail.com>
2017-04-16 23:53:19 -0400

    code touch-up, removed unnecessary global definitions

--- concensus_string.py
+++ concensus_string.py
@@ -1,15 +1,11 @@
-
-
-file2 = 'file.txt'
-
 import numpy as np
 
 def consensus(file):
 
     consensus = []
     letters = ''
-    global dictionary; dictionary = { 'A': 11.0, 'C':22.0, 'G':33.0, 'T':44.0 }
-    global sequence; sequence = {}
+    dictionary = { 'A': 11.0, 'C':22.0, 'G':33.0, 'T':44.0 }
+    sequence = {}
 
     id_num = ''    
     with open(file) as f:
@@ -36,7 +32,7 @@ def consensus(file):
         
         for i in consensus:
             for k,v in dictionary.items():
-                if i ==v:
+                if i == v:
                     letters += k 
         
     return letters
@@ -44,26 +40,25 @@ def consensus(file):
 def occurNumber(array):
     (values,counts) = np.unique(array,return_counts=True)
     max_occur= np.argmax(counts)
- 
     return values[max_occur]    
     
 def maximum(d):
     m=max(d.values())
     M = str([k for k,v in d.items() if v==m][0])
-
     return M  
     
-def main():
+def main(file):
     
-    t = consensus(file2)
+    t = consensus(file)
     concensus_string = t
 
     with open("concensus_string.txt", "w") as text_file:
-        text_file.write(concensus_string + "\n")
-            
+        text_file.write(concensus_string + "\n")  
     text_file.close()
-        
-main()
+ 
+
+file2 = 'file.txt'       
+main(file2)
     
     
     
